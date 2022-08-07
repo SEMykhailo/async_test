3aaa8ad62053e7eb3005469bd09e42121b42e2b9
SEMykhailo <misha15-2009@ukr.net>
2022-08-07 21:19:41 +0300

    Написав шось

--- My_first_bot.py
+++ My_first_bot.py
@@ -1,1 +1,16 @@
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+bot = Bot(token="5471477552:AAE4Szgn46AaL3n0Tos1hDbAFFn8KXEF1iI")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def get_message(message: types.Message):
+    chat_id = message.chat.id
+    text = 'Якийсь текст'
+
+    await bot.send_message(chat_id=chat_id, text=text)
+
+
+executor.start_polling(dp)
