30a7c77ed2cec76fc1001a3eae823a6085e16e95
SEMykhailo <misha15-2009@ukr.net>
2022-10-31 19:08:03 +0200

    commit add test FSM

--- tg_bot/filters/mat_filter.py
+++ tg_bot/filters/mat_filter.py
@@ -1,20 +1,20 @@
 from aiogram import types, Dispatcher
-from aiogram.dispatcher.filters import BoundFilter
+from aiogram.dispatcher.filters import BoundFilter, Filter
 
 
 #from tg_bot.config import Config
 
 
-class MatFilter(BoundFilter):
-    key = 'mat_filter'
+class MatFilter(Filter):
+    #key = 'mat_filter'
     mat_filter = ['хуй', 'хуйло', 'пезда', 'блять']
 
-    def __init__(self, mat_filter: list[str]):
-        if isinstance(mat_filter, list):
-            self.mat_filter = mat_filter
+    # def __init__(self, mat_filter: list[str]):
+    #     if isinstance(mat_filter, list):
+    #         self.mat_filter = mat_filter
 
     async def check(self, message: types.message):
         return any(map(lambda x: x.lower() in self.mat_filter, message.text.split()))
 
-def register_filters(dp: Dispatcher):
-    dp.filters_factory.bind(MatFilter)
+# def register_filters(dp: Dispatcher):
+#     dp.filters_factory.bind(MatFilter)_factory.bind(MatFilter)
