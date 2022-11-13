9ce8cbba60150176fb3344c5b60e65f00f3cb3c2
Mykhailo <misha15-2009@ukr.net>
2022-11-13 21:41:55 +0200

    add def give cat sticker

--- tg_bot/handlers/bot_test.py
+++ tg_bot/handlers/bot_test.py
@@ -1,3 +1,5 @@
+import random
+
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text, Command, state
@@ -23,17 +25,28 @@ async def bot_filter_pytin(message: types.Message):
 async def bot_start(message: types.Message):
     await message.answer(f'Для того, щоб дізнатися свій Telegram id введіть команду /info\n'
                          f'Також цей бот може провести маленьке тестування, для цього введіть команду /test\n'
+                         f'Для того, щоб получити id стікера - просто відправте його в чат\n'
+                         f'Якщо хочете получити випадковий стікер з котиком введіть команду /give\n'
                          f'Крім цього, його можна використовувати, як мат фільтр в чатах, але кількість матів дуже обмежена і це реалізовано просто для тестування фільтрів бота')
 
     #await state.reset_state()
 
-#commit
+async def give_cat_sticker(message: types.Message):
+    STICKER_ID = ['CAACAgIAAxkBAAIBHWNxIcTBQAGIYAkd6ck4PgS3KKKTAAJqAQACFnxoAwWaR4EyB72cKwQ',
+                  'CAACAgIAAxkBAAIBH2NxIlsTQUEOGrpRRzcAAcEceVqB2AACKg8AAmrdSUqUayj3S1F61isE',
+                  'CAACAgIAAxkBAAIBIWNxIl2MuEYtqymUav3kTOPB7J8fAALjCAACXAJlA1UDiUYLzAqHKwQ',
+                  'CAACAgIAAxkBAAIBI2NxImCUiDxTIIpwAWLVOi7pF8iVAAL5CQACFRdZSiAiPE2yrwo1KwQ',
+                  'CAACAgUAAxkBAAIBJWNxImTgmH7iS8snnkGTcJUcx5LHAAKGAgACmLa8LxZxhWGi_gPmKwQ']
+    id_sticker = random.choice(STICKER_ID)
+    await message.bot.send_sticker(message.from_user.id, sticker=id_sticker)
+
 async def  give_id_stickers(message: types.Message):
     await message.answer(message.sticker.file_id)
 
 def register_start(dp: Dispatcher):
     dp.register_message_handler(bot_start, Command('start'), state='*')
     dp.register_message_handler(bot_info, Command('info'))
+    dp.register_message_handler(give_cat_sticker, Command("give"))
     dp.register_message_handler(give_id_stickers, content_types=['sticker'])
     dp.register_message_handler(bot_filter_pytin, Text(contains=FORBIDDEN_PHRASE_1, ignore_case=True))
     dp.register_message_handler(bot_filter_mat, MatFilter())
